9ac8419cbcf6025227cd04386e59b8bd6f06b9ce
Margaret Mwaura <mwauramargaret1@gmail.com>
2020-03-18 16:57:18 +0300

    setting up the user account

--- resources/assets/js/components/MyAccount.vue
+++ resources/assets/js/components/MyAccount.vue
@@ -1,14 +1,22 @@
 <template>
     <div>
 
-        <v-card class="pa-5" v-for="books in getbookscount[0]" :key="books.id">
+        <v-card>
+            <v-flex xs12 md12 >
+                <div>
+                    <p>Below are the books you have Borrowed </p>
+                </div>
+            </v-flex>
+        </v-card>
+
+        <v-card class="pa-5" v-for="(books,index) in getAUsersBooks[0]" :key="index">
             <v-layout row>
                 <v-flex xs12 md12 >
                     <div class="cption grey--text">Book Name</div>
                     <div>{{books.name}}</div>
                 </v-flex>
                     <v-flex xs6 sm2 md2>
-                        <div class="cption grey--text">Book Reserve Date</div>
+                        <div class="cption grey--text">Book Borrow Date</div>
                         <div>{{books.pivot.borrow_date}}</div>
                     </v-flex>
                     <v-flex xs6 sm2 md2>
@@ -19,9 +27,20 @@
                         <div class="cption grey--text">Book Return Date</div>
                         <div>{{books.pivot.return_date}}</div>
                     </v-flex>
+                <v-flex xs6 sm2 md2>
+                    <v-chip>Cancel Book Borrowing</v-chip>
+                </v-flex>
                 </v-layout>
         </v-card>
-        <v-card class="pa-5" v-for="books in getbookscount[1]" :key="books.id">
+
+        <v-card>
+            <v-flex xs12 md12 >
+                <div>
+                    <p>Below are the books you have Reserved </p>
+                </div>
+            </v-flex>
+        </v-card>
+        <v-card class="pa-5" v-for="(books,index) in getAUsersBooks[1]" :key="index">
             <v-layout row>
                 <v-flex xs12 md12 >
                     <div class="cption grey--text">Book Name</div>
@@ -29,15 +48,14 @@
                 </v-flex>
                     <v-flex xs6 sm2 md2>
                         <div class="cption grey--text">Book Reserve Date</div>
-                        <div>{{books.pivot.borrow_date}}</div>
+                        <div>{{books.pivot.reserve_date}}</div>
                     </v-flex>
                     <v-flex xs6 sm2 md2>
                         <div class="cption grey--text">Book Due Date</div>
                         <div>{{books.pivot.due_date}}</div>
                     </v-flex>
                     <v-flex xs6 sm2 md2>
-                        <div class="cption grey--text">Book Return Date</div>
-                        <div>{{books.pivot.return_date}}</div>
+                        <v-chip>Cancel Book Reserving</v-chip>
                     </v-flex>
             </v-layout>
         </v-card>
@@ -50,11 +68,10 @@
     export default {
         name: "MyAccount",
         computed: {
-            ...mapGetters(['getbookscount']),
+            ...mapGetters(['getAUsersBooks']),
         },
         mounted() {
-            this.$store.dispatch('getallusersbooks');
-
+            this.$store.dispatch('getAUsersBooks');
         },
     }
 </script>
